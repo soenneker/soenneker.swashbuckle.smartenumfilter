c321ace6568b7f5cd6375b569177a17849f89444
Jake Soenneker <jake@soenneker.com>
2023-03-21 19:29:18 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Swashbuckle.SmartEnumFilter.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Swashbuckle.SmartEnumFilter/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.swashbuckle.smartenumfilter/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.swashbuckle.smartenumfilter/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Swashbuckle.SmartEnumFilter.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Swashbuckle.SmartEnumFilter/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Swashbuckle.SmartEnumFilter
+### A Swashbuckle filter for SmartEnum
+
+## Installation
+
+```
+Install-Package Soenneker.Swashbuckle.SmartEnumFilter
+```
---
+++ src/SmartEnumSchemaFilter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Ardalis.SmartEnum;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Soenneker.Swashbuckle.SmartEnumFilter;
+
+/// <summary>
+/// A Swashbuckle Schema filter for SmartEnum
+/// </summary>
+public sealed class SmartEnumSchemaFilter : ISchemaFilter
+{
+    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        Type? type = context.Type;
+
+        if (!IsTypeDerivedFromGenericType(type, typeof(SmartEnum<>)) && !IsTypeDerivedFromGenericType(type, typeof(SmartEnum<,>)))
+        {
+            return;
+        }
+
+        IEnumerable<string>? enumValues = type.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.FlattenHierarchy).Select(d => d.Name);
+        var openApiValues = new OpenApiArray();
+        openApiValues.AddRange(enumValues.Select(d => new OpenApiString(d)));
+
+        // See https://swagger.io/docs/specification/data-models/enums/
+        schema.Type = "string";
+        schema.Enum = openApiValues;
+        schema.Properties = null;
+    }
+
+    private static bool IsTypeDerivedFromGenericType(Type? typeToCheck, Type genericType)
+    {
+        while (true)
+        {
+            if (typeToCheck == typeof(object))
+            {
+                return false;
+            }
+
+            if (typeToCheck == null)
+            {
+                return false;
+            }
+
+            if (typeToCheck.IsGenericType && typeToCheck.GetGenericTypeDefinition() == genericType)
+            {
+                return true;
+            }
+
+            typeToCheck = typeToCheck.BaseType;
+        }
+    }
+}
