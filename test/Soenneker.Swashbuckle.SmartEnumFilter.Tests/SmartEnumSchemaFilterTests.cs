6a681b6d09e95fd06e733241330bf0d52b446da3
Jake Soenneker <jake@soeneker.com>
2024-12-23 22:20:15 +0000

    Tests

---
+++ test/Soenneker.Swashbuckle.SmartEnumFilter.Tests/SmartEnumSchemaFilterTests.cs
@@ -0,0 +1,18 @@
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Swashbuckle.SmartEnumFilter.Tests;
+
+[Collection("Collection")]
+public class SmartEnumSchemaFilterTests : FixturedUnitTest
+{
+    public SmartEnumSchemaFilterTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
